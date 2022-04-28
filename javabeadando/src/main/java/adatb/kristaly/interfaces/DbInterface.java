3d2fd678b97476c6257d4faf2662976705a8472b
Csanád Kristály <65303256+kristaly-C@users.noreply.github.com>
2022-04-28 15:13:01 +0200

    del

--- javabeadando/src/main/java/adatb/kristaly/interfaces/DbInterface.java
+++
@@ -1,8 +0,0 @@
-package adatb.kristaly.interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DbInterface {
-    public Connection getConnection() throws SQLException, ClassNotFoundException;
-}
